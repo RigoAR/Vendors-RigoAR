8bdf6ed6ffb7e0510ccee9e273fbf4a6367b2f07
RigoAR <rigobertoalvaradorayo@gmail.com>
2024-11-21 20:34:05 -0600

    Added vendors on main to print them with inventory

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,17 @@
 public class Main {
     public static void main(String[] args) {
+        Vending[] vendors = new Vending[5];
+        vendors[0] = new Vending(10, 5);
+        vendors[1] = new Vending(20, 10);
+        vendors[2] = new Vending(30, 15);
+        vendors[3] = new Vending(5, 25);
+        vendors[4] = new Vending(50, 0);
 
 
-        System.out.println("Welcome to Ultra Shop!");
-
-
-
-        System.out.println("Maintained by <A human>");
-
+        for (int i = 0; i < vendors.length; i++) {
+            System.out.println("Vendor " + (i + 1) + " inventory:");
+            vendors[i].printInventory();
+            System.out.println();
+        }
     }
 }
