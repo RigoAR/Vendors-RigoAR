3e24f0fe3d615a1b1bdc4482ba5e3e006bf45cc0
RigoAR <rigobertoalvaradorayo@gmail.com>
2024-11-21 22:58:44 -0600

    Fixed to add description

--- src/Item.java
+++ src/Item.java
@@ -1,36 +1,30 @@
-public class Item {
+class Item {
     private String name;
     private double price;
     private int stock;
+    private String description;
 
-    public Item(String name, double price, int stock) {
+    public Item(String name, double price, int stock, String description) {
         this.name = name;
         this.price = price;
         this.stock = stock;
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public double getPrice() {
         return price;
     }
 
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
     public int getStock() {
         return stock;
-    }
-
-    public void setStock(int stock) {
-        this.stock = stock;
     }
 
     public void decreaseStock() {
@@ -39,9 +33,7 @@ public class Item {
         }
     }
 
-    public void restock(int amount) {
-        if (amount > 0) {
-            stock += amount;
-        }
+    public void increaseStock(int quantity) {
+        stock += quantity;
     }
 }
