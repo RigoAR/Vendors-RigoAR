d0eac0f202db6fb1a397498ac07cf5f9c566beed
RigoAR <rigobertoalvaradorayo@gmail.com>
2024-11-21 19:53:09 -0600

    Made renameItem method to change name of items

--- src/Vendor.java
+++ src/Vendor.java
@@ -6,70 +6,64 @@ import java.util.HashMap;
  * well as the current balance of money that has been deposited into the machine.
  */
 class Vending {
-    private static HashMap<String, Item> Stock = new HashMap<String,Item>();
+    private static HashMap<String, Item> Stock = new HashMap<String, Item>();
     private double balance;
 
-    Vending(int numCandy, int numGum)
-    {
+    Vending(int numCandy, int numGum) {
         Stock.put("Candy", new Item(1.25, numCandy));
         Stock.put("Gum", new Item(.5, numGum));
         this.balance = 0;
     }
 
-    /** resets the Balance to 0 */
-    void resetBalance ()
-    {
+    /**
+     * resets the Balance to 0
+     */
+    void resetBalance() {
         this.balance = 0;
     }
 
-    /** returns the current balance */
-    double getBalance ()
-    {
+    /**
+     * returns the current balance
+     */
+    double getBalance() {
         return this.balance;
     }
 
-    /** adds money to the machine's balance
+    /**
+     * adds money to the machine's balance
+     *
      * @param amt how much money to add
-     * */
-    void addMoney (double amt)
-    {
+     */
+    void addMoney(double amt) {
         this.balance = this.balance + amt;
     }
 
-    /** attempt to purchase named item.  Message returned if
+    /**
+     * attempt to purchase named item.  Message returned if
      * the balance isn't sufficient to cover the item cost.
      *
      * @param name The name of the item to purchase ("Candy" or "Gum")
      */
-    void select (String name)
-    {
-        if (Stock.containsKey(name))
-        {
+    void select(String name) {
+        if (Stock.containsKey(name)) {
             Item item = Stock.get(name);
-            if (balance >= item.price)
-            {
+            if (balance >= item.price) {
                 item.purchase(1);
                 this.balance = this.balance - item.price;
-            }
-            else
+            } else
                 System.out.println("Gimme more money");
-        }
-        else System.out.println("Sorry, don't know that item");
+        } else System.out.println("Sorry, don't know that item");
     }
 
-    int getStock(String itemName)
-    {
-        if (Stock.containsKey(itemName))
-        {
+    int getStock(String itemName) {
+        if (Stock.containsKey(itemName)) {
             return Stock.get(itemName).stock;
         }
         return -1;
     }
 
-    boolean isInventoryEmpty()
-    {
-        for (Item item : Stock.values())
-        {
+    boolean isInventoryEmpty() {
+        for (Item item : Stock.values()) {
             if (item.stock > 0) {
                 return false;
             }
@@ -77,10 +71,8 @@ class Vending {
         return true;
     }
 
-    void restockItem(String itemName, int amount)
-    {
-        if (Stock.containsKey(itemName))
-        {
+    void restockItem(String itemName, int amount) {
+        if (Stock.containsKey(itemName)) {
             Stock.get(itemName).restock(amount);
         } else {
             Stock.put(itemName, new Item(1.00, amount));
@@ -88,8 +80,21 @@ class Vending {
         }
     }
 
+    public int renameItem(String oldName, String newName) {
+        if (Stock.containsKey(oldName)) {
+            Item item = Stock.get(oldName);
+            Stock.remove(oldName);
+            Stock.put(newName, item);
+            System.out.println("Item renamed from " + oldName + " to " + newName);
+            return 10;
+        } else {
+            System.out.println("Item not found to rename");
+            return -1;
+        }
+    }
 }
 
-class Examples {
+class Examples
+{
 }
 
