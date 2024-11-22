01555d483e7f10cb3d87bee24d596428d4f9f64d
RigoAR <rigobertoalvaradorayo@gmail.com>
2024-11-21 20:33:27 -0600

    Added printInventory method

--- src/Vendor.java
+++ src/Vendor.java
@@ -1,6 +1,5 @@
 import java.util.HashMap;
 
-
 /**
  * Class for a Vending Machine.  Contains a hashtable mapping item names to item data, as
  * well as the current balance of money that has been deposited into the machine.
@@ -11,48 +10,34 @@ class Vending {
 
     Vending(int numCandy, int numGum) {
         Stock.put("Candy", new Item(1.25, numCandy));
-        Stock.put("Gum", new Item(.5, numGum));
+        Stock.put("Gum", new Item(0.5, numGum));
         this.balance = 0;
     }
 
-    /**
-     * resets the Balance to 0
-     */
     void resetBalance() {
         this.balance = 0;
     }
 
-    /**
-     * returns the current balance
-     */
     double getBalance() {
         return this.balance;
     }
 
-    /**
-     * adds money to the machine's balance
-     *
-     * @param amt how much money to add
-     */
     void addMoney(double amt) {
         this.balance = this.balance + amt;
     }
 
-    /**
-     * attempt to purchase named item.  Message returned if
-     * the balance isn't sufficient to cover the item cost.
-     *
-     * @param name The name of the item to purchase ("Candy" or "Gum")
-     */
     void select(String name) {
         if (Stock.containsKey(name)) {
             Item item = Stock.get(name);
             if (balance >= item.price) {
                 item.purchase(1);
                 this.balance = this.balance - item.price;
-            } else
+            } else {
                 System.out.println("Gimme more money");
-        } else System.out.println("Sorry, don't know that item");
+            }
+        } else {
+            System.out.println("Sorry, don't know that item");
+        }
     }
 
     int getStock(String itemName) {
@@ -92,9 +77,12 @@ class Vending {
             return -1;
         }
     }
-}
 
-class Examples
-{
+    void printInventory() {
+        System.out.println("Inventory for Vendor:");
+        for (String itemName : Stock.keySet()) {
+            Item item = Stock.get(itemName);
+            System.out.println(itemName + ": " + item.getStock() + " available");
+        }
+    }
 }
-
