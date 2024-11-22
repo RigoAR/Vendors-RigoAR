b6692266336e202438f31604e8f707c2679a8f24
RigoAR <rigobertoalvaradorayo@gmail.com>
2024-11-21 22:11:15 -0600

    Fixed main to look better and give a rough idea of what it can do

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        Vending[] vendors = new Vending[5];
-        vendors[0] = new Vending(10, 5);
-        vendors[1] = new Vending(20, 10);
-        vendors[2] = new Vending(30, 15);
-        vendors[3] = new Vending(5, 25);
-        vendors[4] = new Vending(50, 0);
-
-
-        for (int i = 0; i < vendors.length; i++) {
-            System.out.println("Vendor " + (i + 1) + " inventory:");
-            vendors[i].printInventory();
-            System.out.println();
-        }
+        Vending machine = new Vending();
+        machine.addMoney(10.00);
+        System.out.println("Balance: " + machine.getBalance());
+        machine.select("Candy");
+        System.out.println("Balance after purchase: " + machine.getBalance());
+        System.out.println("Stock of Candy: " + machine.getStock("Candy"));
     }
 }
